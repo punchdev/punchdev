1871ea68c9d161ca1fdbd99310cf856721bb5b45
Georges Duplessy <gduplessy@gmail.com>
2014-06-15 23:41:51 -0400

    files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = ENV.fetch('ASSETS_VERSION')
+  config.assets.version = 1.0
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
@@ -59,7 +59,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = ENV.fetch('ASSET_HOST')
+  #config.action_controller.asset_host = ENV.fetch('ASSET_HOST')
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
